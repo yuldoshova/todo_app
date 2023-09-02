4e9a0179cf06085121258ede984897841edcf1b9
yuldoshova <yuldoshova002@gmail.com>
2023-09-02 15:04:26 +0500

    create a task servise

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,32 @@
-export class CreateTaskDto {}
+import { IsEnum, IsNotEmpty, IsString } from "class-validator"
+import { Level } from "src/enums/LevelType"
+import { Status } from "src/enums/StatusType"
+
+export class CreateTaskDto {
+
+    @IsString()
+    @IsNotEmpty()
+    task: string
+
+    @IsString()
+    description?: string
+
+    @IsString()
+    date: Date
+
+    @IsString()
+    @IsNotEmpty()
+    start_time: string
+
+    @IsString()
+    @IsNotEmpty()
+    end_time: string
+
+    @IsEnum(Level)
+    @IsNotEmpty()
+    level: Level
+
+    @IsEnum(Status)
+    status?: Status
+
+}
